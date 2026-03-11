5cad48866255cbb059d6b50af0ebfa8c5c9f4396
Jonas B. Mengue <jonas@millenium-seo.com>
2026-03-11 15:45:26 -0300

    feat: ajust banner

--- app/Views/pages/service/sections/banner.php
+++ app/Views/pages/service/sections/banner.php
@@ -1,21 +1,23 @@
 <div class="relative overflow-x-hidden">
   <div class="absolute top-0 left-0 w-full h-full flex justify-center items-end z-1">
     <div class="text-center">
-      <img alt="" title="" src="<?= $appConfig->upload ?>/images/large/<?= $post->logoName ?>" class="block mx-auto mb-8 max-w-[80%]" />
+      <img alt="" title="" src="<?= $appConfig->upload ?>/images/large/<?= $post->logoName ?>" class="block mx-auto mb-8 max-w-[75%]" />
       <h1 class="font-normal text-4xl normal-case"><?= $post->phrase ?></h1>
-
-      <div class="flex flex-wrap justify-center items-center gap-x-10 lg:mt-20 pb-15 md:pb-20 gap-y-5">
-        <a href="<?= $appConfig->openTicketLink ?>" target="_blank" rel="noopener noreferrer" class="btn-secondary btn-lg">Abrir um chamado</a>
-        <a href="tel:<?= $appConfig->phone ?>" class="btn-primary btn-lg py-3 mx-auto bg-white text-[var(--service-color)]">
-            <i class="flaticon-telephone"></i>
-            <?= $appConfig->phone ?>
-          </a>
-        <div data-aos="zoom-in" data-aos-delay="400">
-          <button class="btn-secondary btn-lg w-full lg:w-auto" onclick="<?= esc(
-            whatsapp_conversion_onclick(),
-            'attr',
-          ) ?>">Conversar agora</button>
+      <div class="lg:mt-20 pb-5 md:pb-20">
+        <div class="flex flex-wrap justify-center items-center gap-x-10  gap-y-5">
+          <a href="<?= $appConfig->openTicketLink ?>" target="_blank" rel="noopener noreferrer" class="btn-secondary btn-lg">Abrir um chamado</a>
+          <a href="tel:<?= $appConfig->phone ?>" class="btn-primary btn-lg py-3 mx-auto bg-white text-[var(--service-color)]">
+              <i class="flaticon-telephone"></i>
+              <?= $appConfig->phone ?>
+            </a>
+          <div data-aos="zoom-in" data-aos-delay="400">
+            <button class="btn-secondary btn-lg w-full lg:w-auto" onclick="<?= esc(
+              whatsapp_conversion_onclick(),
+              'attr',
+            ) ?>">Conversar agora</button>
+          </div>
         </div>
+        <div class="text-center mt-5 text-xl" data-aos="fade-up" data-aos-delay="500">Atendimento on-line 24h / 7 dias por semana</div>
       </div>
     </div>
   </div>
